278b429cf3c5de0999aa20399a45b8dd656ad047
isimsizz94 <yakovyakov707@gmail.com>
2022-04-19 21:40:18 +0500

    Add buttons

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import {render} from "./render.js";
 
-setInterval(() => {
+document.querySelector(".selective-render").onclick = () => {
     const persons = document.querySelectorAll(".persons");
 
     console.time("Render");
     render(persons[1], persons[0]);
     console.timeEnd("Render");
-}, 150);
+}
+
+document.querySelector(".outerhtml-render").onclick = () => {
+    const persons = document.querySelectorAll(".persons");
+
+    console.time("Render");
+    persons[1].outerHTML = persons[0].outerHTML;
+    console.timeEnd("Render");
+}
